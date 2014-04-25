579c712c49ea67c1a94fe3e4eecdd9e212d289c5
Abraham R Burnett <abeburnett@gmail.com>
2014-04-24 21:51:22 -0700

    Setup a domain helper on the postItem

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,20 @@
+var postsData = [
+	{
+		title: 'The Unchosen',
+		author: 'Bae Fox',
+		url: 'http://baefox.wordpress.com'
+	},
+	{
+		title: 'The Artifact War',
+		author: 'Bae Fox',
+		http: 'http://baefox.wordpress.com'
+	},
+	{
+		title: "The Goat Herder's Guide to Becoming a Hero",
+		author: 'Bae Fox',
+		http: 'http://baefox.wordpress.com'
+	}
+];
+Template.postsList.helpers({
+	posts: postsData
+});
